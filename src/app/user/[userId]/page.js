a67433b4415773b2f892b8e2604a39cb708dfd5c
gabby <hannahfrench1709@hotmail.com>
2025-10-11 22:04:22 +0100

    style: add styling to user profile page

--- src/app/user/[userId]/page.js
+++ src/app/user/[userId]/page.js
@@ -3,6 +3,7 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { db } from "@/utils/database-connection";
 import Image from "next/image";
+import reelterrorRating from "@/../public/reelterror-rating.png";
 
 export default async function UserIdPage() {
   const user = await currentUser();
@@ -35,43 +36,75 @@ export default async function UserIdPage() {
   const posts = postsResult ? postsResult.rows : [];
 
   return (
-    <div>
-      <h1>{profile.first_name}&apos;s Profile</h1>
+    <section>
+      
+        <div className="signout-container">
+          <SignOutButton>
+            <button className="signout-button">SIGN OUT</button>
+          </SignOutButton>
+        </div>
 
-      <div>
-        <Image
-          src={
-            ["ghostface", "jason", "michaelmyers"].includes(profile.avatar)
-              ? `/${profile.avatar}-avatar-image.jpg`
-              : "/pumpkin-default-avatar-image.jpg"
-          }
-          alt="users avatar"
-          width={100}
-          height={100}
-        />
+        <div className="user-profile-container">
+        <h1 className="user-profile-title">
+          {profile.first_name}&apos;s Profile
+        </h1>
+
+        <div className="user-profile post-wrapper">
+          <Image
+            src={
+              ["ghostface", "jason", "michaelmyers"].includes(profile.avatar)
+                ? `/${profile.avatar}-avatar-image.jpg`
+                : "/pumpkin-default-avatar-image.jpg"
+            }
+            alt="users avatar"
+            width={100}
+            height={100}
+          />
+
+          <p className="user-bio">Bio: {profile.bio}</p>
+        </div>
+
+        <div className="post-wrapper">
+          <h2 className="my-posts-title">MY POSTS</h2>
+          {posts.length === 0 ? (
+            <p>You haven&apos;t made any posts yet.</p>
+          ) : (
+            posts.map((post) => (
+              <div key={post.id} className="post-container">
+                <div className="post-left">
+                  <Image
+                    src={post.poster_url}
+                    alt={post.title}
+                    width={100}
+                    height={120}
+                    className="movie-poster"
+                  />
+                  <h3 className="movie-title">
+                    {post.title} ({post.year})
+                  </h3>
+                </div>
+
+                <div className="post-right">
+                  <p className="user-thoughts">{post.thoughts}</p>
+
+                  <div className="rating-icons">
+                    <p>Rating:</p>
+                    {Array.from({ length: post.rating }).map((_, i) => {
+                      return (
+                        <Image
+                          key={i}
+                          src={reelterrorRating}
+                          alt="ReelTerror rating icon"
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
       </div>
-
-      <p>{profile.bio}</p>
-      <SignOutButton>
-        <button>Sign Out</button>
-      </SignOutButton>
-
-      <section>
-        <h2>My Posts</h2>
-        {posts.length === 0 ? (
-          <p>You haven&apos;t made any posts yet.</p>
-        ) : (
-          posts.map((post) => (
-            <div key={post.id}>
-              <h3>
-                {post.title} ({post.year})
-              </h3>
-              <p>{post.thoughts}</p>
-              <p>Rating: {post.rating}</p>
-            </div>
-          ))
-        )}
-      </section>
-    </div>
+    </section>
   );
 }
